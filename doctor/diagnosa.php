941e2f7bdb56d2ff15bb394cadbd3b9fcf151617
Mrcll <linomarcel399@gmail.com>
2024-11-29 00:35:13 +0700

    Catat Sakit Resep

--- doctor/diagnosa.php
+++ doctor/diagnosa.php
@@ -44,7 +44,7 @@ if($search) {
                         <i class="fas fa-file-medical"></i>
                         <span>Rekam Medis</span>
                     </a>
-                    <a href="patient_queue.php" class="flex items-center space-x-3 p-3 rounded bg-blue-900">
+                    <a href="diagnosa.php" class="flex items-center space-x-3 p-3 rounded bg-blue-900">
                         <i class="fas fa-users"></i>
                         <span>Mencatat Diagnosa</span>
                     </a>
